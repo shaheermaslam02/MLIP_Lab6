6c47d34f35e8c66101ba9cea841d020850d73dc8
Shaheer Aslam <shaheermaslam20@gmail.com>
2024-10-04 11:32:29 -0400

    Venv udpate

--- mlip/bin/activate.fish
+++ mlip/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/saslam/mlip"
+set -gx VIRTUAL_ENV "/home/saslam/MLIP_Lab6/mlip"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
